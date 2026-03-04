536643302831ea11d6b2feb34dfc904e86f1df21
Iro-Pliskin <123872414+iro-pliskin@users.noreply.github.com>
2026-03-04 19:53:46 +0000

    Added support for INI config files
    Removed the hardcoded skill values in favour of modular INI defined values.

--- src/settings.cpp
+++ src/settings.cpp
@@ -3,8 +3,39 @@
 
 void Settings::LoadSettings() 
 {
-    if (RE::TESDataHandler::GetSingleton()->LookupLoadedModByName("HandtoHand.esp"sv)) {
-        logger::info("Hand to Hand detected.");
-        h2h_present = true;        
+    logger::info("Loading settings...");
+    
+    CSimpleIniA ini;
+    ini.SetUnicode();
+
+    constexpr auto iniPath = R"(.\Data\SKSE\Plugins\LevelLockedPicking.ini)";
+
+    SI_Error rc = ini.LoadFile(iniPath);
+    ini.LoadFile(iniPath);
+
+    // If the ini file isn't found, it just uses the hardcoded values in settings.h
+    if (rc < 0) {
+       logger::warn("INI file not found in {}, using default values", iniPath);
     }
+    
+    debug_logging = ini.GetBoolValue("Logging", "DebugLogging");
+
+    novice_skill = ini.GetLongValue("SkillRequirements", "iNoviceLockRequirement", novice_skill);
+    apprentice_skill = ini.GetLongValue("SkillRequirements", "iApprenticeLockRequirement",apprentice_skill);
+    adept_skill = ini.GetLongValue("SkillRequirements", "iAdeptLockRequirement", adept_skill);
+    expert_skill = ini.GetLongValue("SkillRequirements", "iExpertLockRequirement", expert_skill);
+    master_skill = ini.GetLongValue("SkillRequirements", "iMasterLockRequirement", master_skill);
+
+
+    if (debug_logging) {
+        spdlog::set_level(spdlog::level::debug);
+        logger::debug("Debug logging enabled");
+    }
+    
+    if (RE::TESDataHandler::GetSingleton()->LookupLoadedModByName("HandtoHand.esp"sv)) {
+        logger::info("Hand to Hand detected");
+        h2h_present = true;
+    }
+
+    logger::info("Settings loaded");
 }
